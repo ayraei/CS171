08ac205d9107f24c2bf6dddc8c51187d6f7ddf6d
Joanne Li <joanne@caltech.edu>
2015-10-07 03:28:17 -0700

    Added file for HW0:Part2.

---
+++ HW0/Part2/eigen_demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ifstream>
+#include <Eigen/Dense>
+
+using namespace Eigen;
+
+int main(int argc, char* argv)
+{
+    MatrixXd mIn(3,3);
+    MatrixXd mCurr(3,3);
+    MatrixXd 
+    return 0;
+}
